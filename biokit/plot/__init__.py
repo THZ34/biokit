5ca260da8f973bc8642c2ac0a21e3cb6190968a4
唐宏振 <tanghongzhen34@gmail.com>
2023-02-16 15:55:11 +0800

    Changes

--- biokit/plot/__init__.py
+++ biokit/plot/__init__.py
@@ -5,7 +5,7 @@ from ._fisher_bar import fisher_bar, ttest_box
 from ._fit_models import flow_chart
 from ._heatmap import heatmap
 from ._kaplan_meier import kaplan_meier
-from ._roc_plot import roc_plot
+from ._roc_plot import rocplot,rocplots
 from ._variants_landscape import oncoplot
 from ._volcano_plot import volcano_plot
 from ._lasso import lassocv

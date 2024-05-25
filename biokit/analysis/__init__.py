1e04a24f056bc8676dcd4b4cfbe77c5b2b97e8f3
tanghongzhen <tanghongzhen34@gmail.com>
2024-05-25 11:33:40 +0800

    新增：基于docker的msbio(metascape镜像)执行通路富集

--- biokit/analysis/__init__.py
+++ biokit/analysis/__init__.py
@@ -3,3 +3,4 @@ from ._fit_models import *
 from ._group4 import group4
 from ._signature_score import signature_score
 from ._cell_annotation import *
+from ._pathway_enrichment import pathway_enrichment
--- biokit/analysis/_pathway_enrichment.py
+++ biokit/analysis/_pathway_enrichment.py
@@ -9,5 +9,5 @@ def pathway_enrichment(genes, prefix, outputdir):
     with open(f'{prefix}.txt', 'w') as f:
         f.write('\n'.join(genes))
     command = f'docker run -v "$(pwd)":/workdir -w /workdir metadocker8/msbio2 python /msbio/mylib/ms/msbio2.py /workdir/{prefix}.txt' \
-              f' -o /workdir/{outputdir}/prefix -t Symbol -s -u --license /workdir/license'
+              f' -o /workdir/{outputdir}/{prefix} -t Symbol -s -u --license /workdir/license'
     return command

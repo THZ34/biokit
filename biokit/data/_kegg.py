62b30e60bdf235032a23e115e447bbb896a61674
tanghongzhen <tanghongzhen34@gmail.com>
2024-05-29 17:33:44 +0800

    新增：获取kegg通路中的基因

--- biokit/data/_kegg.py
+++ biokit/data/_kegg.py
@@ -2,12 +2,22 @@
 # Author:Tang Hongzhen
 # Email: tanghongzhen34@gmail.com
 
+import os
+import json
+import requests
 
-# curl -s http://togows.dbcls.jp/entry/pathway/hsa00010/genes.json \
-#     | awk 'BEGIN{OFS="\t"}$0~/KO:/{
-#         match($0,/"([^"]+)": "([^;[]+);?(.+?) \[(KO:[^\]]+)/,a);
-#         a[3]=a[3]==""?"-":a[3];
-#         print a[1],a[2],a[3],a[4]
-#         }' \
-#     | head -10
-
+def download_kegg_pathway(hsaid,datapath):
+    """Download KEGG pathway data by hsa id"""
+    if not os.path.exists(os.path.join(datapath, f'{hsaid}.json')):
+        os.makedirs(datapath)
+        url = f'http://togows.dbcls.jp/entry/pathway/{hsaid}/genes.json'
+        r = requests.get(url)
+        with open(os.path.join(datapath, f'{hsaid}.json'), 'w') as f:
+            f.write(r.text)
+        pathway_gene_dict = json.loads(r.text)
+        pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict.items() if v}
+    else:
+        with open(os.path.join(datapath, f'{hsaid}.json')) as f:
+            pathway_gene_dict = json.load(f)
+            pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict.items() if v}
+    return pathway_gene_dict

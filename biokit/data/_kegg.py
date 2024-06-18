f33aa8c6618b2744932b960d0ce06f803b197f4c
tanghongzhen <tanghongzhen34@gmail.com>
2024-06-18 13:54:30 +0800

    新增：检查本地文件，避免重复下载

--- biokit/data/_kegg.py
+++ biokit/data/_kegg.py
@@ -6,18 +6,19 @@ import os
 import json
 import requests
 
-def download_kegg_pathway(hsaid,datapath):
+datapath = os.path.join(os.path.dirname(__file__), 'kegg')
+def download_kegg_pathway(hsaid, datapath=datapath):
     """Download KEGG pathway data by hsa id"""
     if not os.path.exists(os.path.join(datapath, f'{hsaid}.json')):
-        os.makedirs(datapath)
+        os.makedirs(datapath,exist_ok=True)
         url = f'http://togows.dbcls.jp/entry/pathway/{hsaid}/genes.json'
         r = requests.get(url)
         with open(os.path.join(datapath, f'{hsaid}.json'), 'w') as f:
             f.write(r.text)
         pathway_gene_dict = json.loads(r.text)
-        pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict.items() if v}
+        pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict[0].items() if v}
     else:
         with open(os.path.join(datapath, f'{hsaid}.json')) as f:
             pathway_gene_dict = json.load(f)
-            pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict.items() if v}
+            pathway_gene_dict = {k: v.split(';')[0] for k, v in pathway_gene_dict[0].items() if v}
     return pathway_gene_dict

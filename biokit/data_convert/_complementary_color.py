1f95edef131b8ed43f8e274abbdd0f44c90c485d
tanghongzhen <tanghongzhen34@gmail.com>
2024-11-01 10:25:57 +0800

    新增: 生成互补色

---
+++ biokit/data_convert/_complementary_color.py
@@ -0,0 +1,17 @@
+# coding='utf-8'
+# Author:Tang Hongzhen
+# Email: tanghongzhen34@gmail.com
+
+import matplotlib.colors as mcolors
+
+
+def complementary_color(color: str | tuple) -> str:
+    """
+    获取互补色
+    :param color: 16进制颜色或RGB颜色
+    :return: color的互补色
+    """
+    if isinstance(color, str):
+        rgb = mcolors.hex2color(color)
+    cmy = [(1 - x) for x in rgb]
+    return mcolors.rgb2hex(cmy)

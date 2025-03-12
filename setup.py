d41c0f928be5cb3fc3dd24c1ef07b8c26c85a178
tanghongzhen <tanghongzhen34@gmail.com>
2025-03-12 16:49:27 +0800

    修改: 将biokit-cli加入环境变量

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
-import setuptools
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+from setuptools import setup, find_packages
+
+setuptools.setup()
+
+setup(
+    name="biokit",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "biokit-cli = biokit.cli:main"  # 入口指向 cli.py 的 main 函数
+        ]
+    },
+    setup_requires=['pbr'],
+    pbr=True
+)

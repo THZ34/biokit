79209b02f571a3190b4913b6831ca0e2e97ea858
tanghongzhen <tanghongzhen34@gmail.com>
2025-03-12 18:01:33 +0800

    Change

--- biokit/cli.py
+++ biokit/cli.py
@@ -5,22 +5,34 @@
 
 
 import argparse
+import fire
+from shell import init_conda
+from shell import set_fontpath
+from shell import set_index_url
 
 
-def main():
-    parser = argparse.ArgumentParser(description="BioKit 命令行工具")
-    subparsers = parser.add_subparsers(dest="command", help="子命令")
+class BioKitCLI:
+    """BioKit 命令行工具"""
 
-    # 示例子命令：序列分析
-    parser_seq = subparsers.add_parser("seq", help="序列分析")
-    parser_seq.add_argument("-i", "--input", required=True, help="输入文件")
-    parser_seq.add_argument("-o", "--output", help="输出文件")
+    def __init__(self):
+        self.conda = CondaCommands()
+        self.matplotlib = MatplotlibCommands()
 
-    args = parser.parse_args()
 
-    if args.command == "seq":
-        print(f"处理序列: {args.input} -> {args.output}")
+class CondaCommands:
+    def __init__(self):
+        self.init = init_conda
+
+
+class MatplotlibCommands:
+    def __init__(self):
+        self.set_fontpath = set_fontpath
+
+
+class PythonCommands:
+    def __init__(self):
+        self.set_index_url = set_index_url
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(BioKitCLI)
--- biokit/shell/__init__.py
+++ biokit/shell/__init__.py
@@ -2,3 +2,6 @@
 # Author:Tang Hongzhen
 # Email: tanghongzhen34@gmail.com
 # %%
+from ._conda import init_conda
+from ._matplotlib import set_fontpath
+from ._python import set_index_url

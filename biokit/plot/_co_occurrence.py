55226f6b72585c37c6023c1c4c6ae53f265dbd8e
tanghongzhen <tanghongzhen34@gmail.com>
2025-04-13 14:13:39 +0800

    Change

--- biokit/plot/_co_occurrence.py
+++ biokit/plot/_co_occurrence.py
@@ -9,7 +9,8 @@ from data._co_occurrence import get_co_occurrence_df
 from plot import create_fig
 
 
-def cooccurrence_lineplot(adata, groupby, groups1=None, groups2=None, color_dict=None):
+def cooccurrence_lineplot(adata, groupby, groups1=None, groups2=None, color_dict=None, lineparams=None,
+                          legendparams=None):
     if groups1 is None:
         groups1 = adata.obs[groupby].cat.categories
     if groups2 is None:
@@ -19,14 +20,12 @@ def cooccurrence_lineplot(adata, groupby, groups1=None, groups2=None, color_dict
         color_dict = dict(zip(groups, sns.color_palette('Set2', n_colors=len(groups))))
 
     co_occurrence_df = get_co_occurrence_df(adata, groupby)
-    fig, ax = create_fig(12, 4, len(groups1), 1)
-    for group1 in groups1:
+    fig, axes = create_fig(12, 4, len(groups1), 1)
+    for group1, ax in zip(groups1, axes.flatten()):
         for group2 in groups2:
             temp_cooccurrence_df = co_occurrence_df[(co_occurrence_df['group1'] == group1) & (
                     co_occurrence_df['group2'] == group2)]
-            ax.plot(temp_cooccurrence_df.columns[2:],
-                    temp_cooccurrence_df.values[0][2:],
-                    color=color_dict[group2], label=group2)
-            ax.legend(bbox_to_anchor=(1, 1), loc='upper left')
+            ax.plot(temp_cooccurrence_df.columns[2:], temp_cooccurrence_df.values[0][2:], color=color_dict[group2],
+                    label=group2, **lineparams)
+            ax.legend(bbox_to_anchor=(1, 1), loc='upper left', **legendparams)
             plt.tight_layout()
-            plt.show()

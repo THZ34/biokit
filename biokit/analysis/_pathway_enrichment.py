98bf9b9c5287bfff858ca906a423921bd8a5ff8a
tanghongzhen <tanghongzhen34@gmail.com>
2025-06-03 10:35:28 +0800

    修改: docker运行命令增加-rm参数在使用后自动销毁容器

--- biokit/analysis/_pathway_enrichment.py
+++ biokit/analysis/_pathway_enrichment.py
@@ -12,9 +12,11 @@ def pathway_enrichment(genes, prefix, outputdir, license=None):
         f.write('\n'.join(genes))
     if license is None:
         license = "$(pwd)/license"
-    command = (f'docker run -u "$(id -u)" '
+    command = (f'docker run --rm -u "$(id -u)" '
                f'-v "$(pwd)":/workdir '
                f'-v "{license}":/workdir/license '
                f'-w /workdir metadocker8/msbio2 python /msbio/mylib/ms/msbio2.py "/workdir/{outputdir}/{prefix}.txt" '
                f'-o "/workdir/{outputdir}/{prefix}" -t Symbol -s -u --license /workdir/license')
     return command
+
+
